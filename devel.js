58a1bc7bcf8e6f97924bdd91f1ad762117452f09
Joshua LeBlanc <joshleblanc94@gmail.com>
2019-12-29 19:19:30 -0400

    simplify

--- devel.js
+++ devel.js
@@ -2,9 +2,6 @@ define([
     './testing',
     'amber/devel',
     // --- packages used only during development begin here ---
-    'amber/legacy/Benchfib',
-    'amber/legacy/Examples',
-    'amber/legacy/IDE'
     // --- packages used only during development end here ---
 ], function (amber) {
     return amber;
--- lambda/index.js
+++
@@ -1,4 +0,0 @@
-var app = require('./the.js')('TestBackend');
-
-exports.status = app('status');
-exports.echo = app('echoEvent:andContext:');
